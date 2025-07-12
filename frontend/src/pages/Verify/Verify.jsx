1644dcd331731a6f96caa40b8aa2857655191ed0
Samar Yadav <samarpratapjnv046@gmail.com>
2025-07-12 17:06:42 +0530

    error-solved

--- frontend/src/pages/Verify/Verify.jsx
+++ frontend/src/pages/Verify/Verify.jsx
@@ -9,6 +9,7 @@ const Verify = () => {
     const [searchParams,setSearchParams] =useSearchParams();
     const success =searchParams.get("success")
     const orderId =searchParams.get("orderId")
+
     const {url} =useContext(StoreContext);
     const navigate  = useNavigate();
 
