02012dbd493cbefaaea5628164fc4c65cb6b3545
Samar Yadav <samarpratapjnv046@gmail.com>
2025-07-13 11:02:16 +0530

    vite-config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,20 +6,41 @@
 //   plugins: [react()],
 // })
 
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//     open: true,
+//     fs: {
+//       strict: false
+//     },
+//     historyApiFallback: true // 👈 ADD THIS
+//   },
+//   build: {
+//     outDir: 'dist'
+//   }
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    open: true,
-    fs: {
-      strict: false
-    },
-    historyApiFallback: true // 👈 ADD THIS
+    port: 3000,  // fix your local port if needed
   },
   build: {
-    outDir: 'dist'
+    outDir: 'build'
+  },
+  base: '/',  // set base path
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
   }
 })
